e51524a7c9b4a271b532be4fd0f8ee2dbfcd0529
megrose10 <157438545+megrose10@users.noreply.github.com>
2024-12-09 19:11:33 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,29 @@
 function augmentingPath(graph, start, end) {
+    let pathToEnd = [];
+    pathToEnd.push(start);
+    let nextNode;
+    let notVisited = true;
+    for(let node in graph) {
+        for(let nodeElement in node) {
+            for(let i = 0; i < pathToEnd.length; i++) {
+                if(pathToEnd[i] == nodeElement){
+                    break;
+                }
+                else{
+                    notVisited = true;
+                }
+            }
+            if(notVisited == true) {
+                pathToEnd.push(node);
+                nextNode = nodeElement;
+            }
+
+        }
+        if(node == end) {
+            return pathToEnd;
+        }
+        //console.log(node);
+    }
+
     return [];
 }
